7f1f2b3f330fbc7c3cc4cd3b1db1ab5bb6511dec
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-27 21:52:41 -0300

    primeira funcao de interacao com o usuario

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 //externos
-const inquirer = require('inquirer');
-const chalk = require('chalk');
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 //core
-const fs = require('fs');
+const fs = require("fs");
 
-console.log('Inicio do projeto accounts')
+const operation = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "O que você gostaria de fazer?",
+      choices: [
+        "Criar conta",
+        "Consultar saldo",
+        "Depositar",
+        "Sacar",
+        "Sair",
+      ],
+    },
+  ])
+  .then((answer) => {
+    const action = answer['action'];
+    console.log(action);
+  })
+  .catch(err => console.error(err))
+};
+
+operation();
